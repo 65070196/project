8ec9bb95094f11b498e73a470c86647d7ad1410c
65070196 <117916797+65070196@users.noreply.github.com>
2026-03-26 00:25:28 +0700

    fix some bug

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# เปลี่ยนคำว่า 'myproject' เป็นชื่อโฟลเดอร์โปรเจกต์ของคุณ (โฟลเดอร์ที่มีไฟล์ settings.py)
+# ตรงนี้สำคัญมาก ต้องเป็น 'project.settings' ครับ
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-# Vercel จะมองหาตัวแปรที่ชื่อว่า 'app' เพื่อใช้รันเซิร์ฟเวอร์
 app = get_wsgi_application()
