83e0eb9a7b0a696f6355f985eda3f3170897b25b
65070196 <117916797+65070196@users.noreply.github.com>
2026-03-26 14:54:13 +0700

    fix some bug

--- project/wsgi.py
+++ project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+# เพิ่มบรรทัดนี้ลงไป เพื่อแปลภาษาให้ Vercel เข้าใจ!
+app = application
