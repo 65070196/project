2abee56501b413e90dc48c41125d64a6c6fdc173
65070196 <117916797+65070196@users.noreply.github.com>
2026-03-27 18:01:07 +0700

    fix

--- project/settings.py
+++ project/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True
     )
 }
